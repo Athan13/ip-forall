0c4d926b1c9458f2f25445a730c3ea214af3b84a
Athan Massouras <amassouras13@gmail.com>
2026-05-14 00:51:53 -0700

    typechecking works
    Signed-off-by: Athan Massouras <amassouras13@gmail.com>

--- app/Main.hs
+++ app/Main.hs
@@ -10,16 +10,24 @@ import Typecheck (inferType)
 import Environment (runTCMonad)
 import Data.List (intercalate)
 
--- The identity function for types: λx. x (or, in de Bruijn notation, λ. 0)
-e0 :: Term Z
-e0 = Lam (LocalName "x" `bind1` Var 0)
+-- The identity function for types: `λ(x : Type). x`
+-- In de Bruijn notation, `λ. 0`
+ex0 :: Term Z
+ex0 = Ann
+    (Lam (LocalName "x" `bind1` Var 0))
+    (TyPi TyType (LocalName "Type" `bind1` TyType))
 
--- The identity function for values: λ(y : Type). λ(x : y). x (or, in de Bruijn, λ. λ. 0)
-e1 :: Term Z
-e1 = Lam (LocalName "y" `bind1` Lam (LocalName "x" `bind1` Var 0))
+-- The identity function for values: `λ(y : Type). λ(x : y). x`
+-- In de Bruijn: `λ. λ. 0`
+ex1 :: Term Z
+ex1 = Ann
+    (Lam (LocalName "y" `bind1` Lam (LocalName "x" `bind1` Var 0)))
+    (TyPi TyType (LocalName "y" `bind1`
+        TyPi (Var 0) (LocalName "x" `bind1` Var 1)
+    ))
 
 examples :: [Term Z]
-examples = [e0, e1]
+examples = [ex0, ex1]
 
 typeCheckExpr :: Term Z -> String
 typeCheckExpr term = case runExcept $ runTCMonad $ inferType term of
